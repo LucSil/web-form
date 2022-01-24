8f5768091dd8362a5c8dd709fd59c41cf473b8f8
LucSil <luciansilva351@gmail.com>
2022-01-24 09:00:27 +0100

    added all files necessary for the project

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template></template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #444;
+  margin-top: 60px;
+}
+#app h1 {
+  width: 400px;
+  margin: 0 auto 20px auto;
+  border-bottom: 1px solid #ddd;
+  padding-bottom: 10px;
+}
+</style>
